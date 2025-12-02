85b449813603493e0365fef352107deb574f8973
Kyle <kmthayer@uw.edu>
2025-12-02 12:36:23 -0800

    finish ejs templating demo

--- lecture/l-18-templating/demo-1-ejs-end/routes/index.js
+++ lecture/l-18-templating/demo-1-ejs-end/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  isLoggedIn = true // pretend this is loaded from session
+
+  storeItems = [
+    {name: "apple", price: 2},
+    {name: "orange", price: 1.5},
+    {name: "m&ms", price: 1.5}
+  ]
+
+  res.render('index', { 
+    title: 'EJS store example' ,
+    isLoggedIn: isLoggedIn,
+    storeItems: storeItems
+  });
 });
 
 module.exports = router;
